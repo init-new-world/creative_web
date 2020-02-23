9152990887d4db1333d9d6ac811f048f8c8e212d
Init_new_world <init.new.world@gmail.com>
2020-02-23 19:49:48 +0800

    Add layout details

---
+++ control/front/options.py
@@ -0,0 +1,7 @@
+from control.front.structs import *
+
+title = "科技创新网"
+
+nav_list = [Navigator(label='about',url='/about',option='user',name='关于')]
+
+github_url = 'https://github.com/init-new-world/creative_web'
---
+++ control/front/structs.py
@@ -0,0 +1,6 @@
+class Navigator:
+    def __init__(self,label,url,option,name):
+        self.label = label
+        self.url = url
+        self.option = option
+        self.name = name
